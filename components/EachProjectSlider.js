093599a096b2cc150e388a83a942e2c907b2aa8e
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-01-20 12:36:41 +0100

    added swipe

--- components/EachProjectSlider.js
+++ components/EachProjectSlider.js
@@ -150,15 +150,15 @@ const EachProjectSlider = ({ project, index }) => {
                     href={`/projects/${project._id}`}
                     className="flex items-center justify-between gap-3"
                   >
-                    <svg
+                    {/* <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
                       className="w-6 h-6"
                     >
                       <path d="M4.5 4.5a3 3 0 00-3 3v9a3 3 0 003 3h8.25a3 3 0 003-3v-9a3 3 0 00-3-3H4.5zM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06z" />
-                    </svg>
-                    <span className="font-raleway">Take a 3D Tour</span>
+                    </svg> */}
+                    <span className="font-raleway">Swipe left</span>
                   </Link>
                 </div>
                 {/* <div className="absolute left-0 flex w-full items-end justify-start bottom-0 py-5 md:py-10 my-auto px-16 md:h-40 glass-bg2 text-white">
